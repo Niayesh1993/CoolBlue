412c2725f4713f7eedeaa067fe1b1fbb7b35cc8a
admin <niayesh1993@gmail.com>
2021-12-29 18:42:49 +0330

    initial commit

---
+++ app/src/main/java/xyz/zohre/coolblue/MainActivity.kt
@@ -0,0 +1,12 @@
+package xyz.zohre.coolblue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coolblue.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
