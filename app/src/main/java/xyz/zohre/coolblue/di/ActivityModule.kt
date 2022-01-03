ed73981c9b5b42255e656d4e5560f6c65d1d4797
admin <niayesh1993@gmail.com>
2022-01-03 11:51:13 +0330

    Code Refactor

--- app/src/main/java/xyz/zohre/coolblue/di/ActivityModule.kt
+++ app/src/main/java/xyz/zohre/coolblue/di/ActivityModule.kt
@@ -1,13 +1,10 @@
 package xyz.zohre.coolblue.di
 
-import android.content.Context
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.android.qualifiers.ActivityContext
 import xyz.zohre.presentation.AppNavigator
 
 @InstallIn(ActivityComponent::class)
@@ -18,8 +15,5 @@ class ActivityModule {
         return coolBlueAppNavigator
     }
 
-    @Provides
-    fun provideLocationClient(@ActivityContext context: Context): FusedLocationProviderClient {
-        return LocationServices.getFusedLocationProviderClient(context)
-    }
+
 }
